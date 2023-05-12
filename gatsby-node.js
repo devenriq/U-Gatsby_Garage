381a72b9ce8830cb18736a7466e8f3a4eb439290
enrique palomino <octepc.97@gmail.com>
2023-05-12 16:03:06 -0500

    feat: :wrench: Working on the configuration to render the info from the worpdress tool

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,18 @@
+//Esto permite el uso de path para referenciar localizaciones en el proyecto
 const path = require("path");
+//Esto permite el uso de una herramienta que genera ids automáticamente
+const { assignIds } = require("@webdeveducation/wp-block-tools");
+//Permite el acceso a archivos del sistema
+const fs = require("fs");
 
 exports.createPages = async ({ actions, graphql }) => {
   const pageTemplate = path.resolve("src/templates/page.js");
   const { createPage } = actions;
   const { data } = await graphql(`
     query AllPagesQuery {
+      wp {
+        themeStylesheet
+      }
       allWpPage {
         nodes {
           databaseId
@@ -15,11 +23,22 @@ exports.createPages = async ({ actions, graphql }) => {
     }
   `);
 
+  try {
+    fs.writeFileSync("./public/themeStylesheet.css", data.wp.themeStylesheet);
+  } catch (error) {
+    console.log(error);
+  }
+
   for (let i = 0; i < data.allWpPage.nodes.length; i++) {
     const page = data.allWpPage.nodes[i];
+    let blocks = page.blocks;
+    blocks = assignIds(blocks);
     createPage({
       path: page.uri,
       component: pageTemplate,
+      context: {
+        blocks,
+      },
     });
   }
 };
--- src/templates/page.js
+++ src/templates/page.js
@@ -1,7 +1,42 @@
 import React from "react";
+import {
+  BlockRendererProvider,
+  BlockRenderer,
+  getStyles,
+  getClasses,
+} from "@webdeveducation/wp-block-tools";
 
-const Page = () => {
-  return <div>This is a page template</div>;
+const Page = (props) => {
+  console.log(props);
+  return (
+    <BlockRendererProvider
+      allBlocks={props.pageContext.blocks}
+      renderComponent={(block) => {
+        console.log(block);
+        switch (block.name) {
+          case "core/media-text": {
+            const content = <BlockRenderer blocks={block.innerBlocks} />;
+
+            return (
+              <div
+                key={block.id}
+                style={getStyles(block)}
+                className={getClasses(block)}
+              >
+                {block.attributes.mediaPosition === "right" && (
+                  <div>{content}</div>
+                )}
+                <div>This will be the image</div>
+                {block.attributes.mediaPosition !== "right" && (
+                  <div>{content}</div>
+                )}
+              </div>
+            );
+          }
+        }
+      }}
+    />
+  );
 };
 
 export default Page;
